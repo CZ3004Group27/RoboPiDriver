864820962e9009af0fd7841a4cf975bd36a12f91
RobedPixel <18674579+robedpixel@users.noreply.github.com>
2022-09-02 09:14:22 +0800

    added os switch for STM module

--- src/STMModule.py
+++ src/STMModule.py
@@ -1,10 +1,19 @@
 from multiprocessing import Process, Queue
 from Action import *
+import os
 
+if os.name() == 'nt':
 
-class STMModule:
-    def __init__(self):
-        pass
+    class STMModule:
+        def __init__(self):
+            pass
 
-    def process_move(self, move: Action):
-        pass
+        def process_move(self, move: Action):
+            pass
+else:
+    class STMModule:
+        def __init__(self):
+            pass
+
+        def process_move(self, move: Action):
+            pass
