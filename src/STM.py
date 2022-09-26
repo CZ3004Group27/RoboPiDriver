08e1967794182afe0f12da60882cc0ec418b8399
josedgefield <putturshreyas@gmail.com>
2022-09-26 14:58:04 +0800

    rpi stm conn. established

--- src/STM.py
+++ src/STM.py
@@ -2,6 +2,11 @@ from Action import *
 from STMModule import STMModule
 
 stm = STMModule()
+
+stm.connect()
+
+stm.forward()
+
 stm.process_move(RobotAction.FORWARD,0,0,0)
 
 stm.process_move(RobotAction.TURN_FORWARD_LEFT,0,0,0)
@@ -11,3 +16,5 @@ stm.process_move(RobotAction.TURN_FORWARD_RIGHT,0,0,0)
 stm.process_move(RobotAction.TURN_BACKWARD_LEFT,0,0,0)
 
 stm.process_move(RobotAction.TURN_BACKWARD_RIGHT,0,0,0)
+
+stm.disconnect()
