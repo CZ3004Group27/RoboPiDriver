96363778b9421bc110d14d7ab2ea33b525c19ea7
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2022-09-30 16:24:18 +0800

    experimental picamera module updates

--- src/PiCameraModule.py
+++ src/PiCameraModule.py
@@ -7,19 +7,21 @@ class CameraModule:
             self.camera_index = 0
             self.initialised = False
             self.camera = Picamera2()
-            self.camera.resolution = (640,480)
-            self.camera.framerate = 30
+
+            config = picam2.still_configuration(main={"size": (640, 480)}, "format": "BGR888"})
+
+            self.camera.configure(config)
+            self.camera.video_configuration.controls.framerate = 30
 
             # Make the image taken brighter
             self.camera.set_controls({"Brightness": 0.5})
-            self.image = np.empty((640 * 480 * 3,), dtype=np.uint8)
+            self.camera.start()
 
             # allow the camera to warmup
-            time.sleep(0.1)
+            time.sleep(1)
             self.initialised = True
 
         def take_picture(self):
             # grab an image from the camera
-            self.camera.capture(image, 'bgr')
-            image = image.reshape((640, 480, 3))
+            image = self.camera.capture_array("main")
             return image
